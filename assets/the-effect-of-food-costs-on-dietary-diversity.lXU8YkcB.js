a2103b2a2634fd7c052773edd22ada1a19bce312
Adolf Alfred <adolfalfred321@gmail.com>
2025-08-16 17:24:16 +0300

    Updates

---
+++ assets/the-effect-of-food-costs-on-dietary-diversity.lXU8YkcB.js
@@ -0,0 +1,18 @@
+const e=`# The Effect of Food Costs on Dietary Diversity: Insights from Rural and Urban Households, Gender Dynamics, and Agricultural Involvement in Tanzania\r
+\r
+**MJ Chegere, DE Macha - VeriXiv, 2025**\r
+\r
+This study investigates the relationship between food costs and dietary diversity across different demographic and geographic contexts, with a focus on agricultural involvement, gender dynamics, and urban-rural differences. s. Using data from the fourth and fifth waves of the National Panels Survey of Tanzania, analysis of household consumption patterns shows the prevalence of staple foods such as cereals, and vegetables, with affordability of a balanced diet emerging as a critical issue, often surpassing household budgets, especially for low-income families.\r
+\r
+Employing Mundlak Correlated Random Effects models, we analyze the relationship between the cost of a balanced diet plate and dietary diversity measures, including the Food-Based Dietary Guidelines (FBDG), Food Consumption Score (FCS), and Household Dietary Diversity Score (HDDS). Our findings reveal that higher food costs are negatively associated with dietary diversity, with a pronounced effect in rural areas compared to urban settings. Gender dynamics, particularly the role of joint versus individual planting decisions, also influence dietary diversity, with more inclusive decision-making associated with better dietary outcomes. Robustness checks, including alternative dietary measures and Poisson fixed effects models, confirm the reliability of our results. This study highlights the need for targeted interventions to address food cost sensitivity, particularly in rural areas, and emphasizes the role of gender and agricultural practices in shaping dietary patterns.\r
+\r
+**Authors:** Martin J Chegere, Doris E Macha\r
+\r
+**Publication date:** 2025/6/11\r
+\r
+**Journal:** VeriXiv\r
+\r
+**Publisher:** F1000 Research Limited\r
+\r
+[Read more from source](https://verixiv.org/articles/2-122/v1?src=rss)\r
+`;export{e as default};
